cbdf85f1eb5813d1e81666faa8f3e04cdbd01bab
Ahmed MohamedOsman Ahmed Omer <ahmed.omer.apu@gmail.com>
2025-12-05 23:24:53 +0800

    Start xinetd and custom UDP/TCP services to allow practicing a wider range of Nmap scan types and scripts.

--- challenges/nmap-ctf-20251205-213500/entrypoint.sh
+++ challenges/nmap-ctf-20251205-213500/entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+set -e
+
 service ssh start
-service nginx start
 service vsftpd start
-service smbd start || service samba start
-# Dummy TCP service on 9999
-while true; do echo "Nmap training port" | nc -l -p 9999; done &
-/bin/bash
+service apache2 start
+service smbd start || service samba start || true
+
+# Start inetd/xinetd for extra TCP services
+service xinetd start || true
+
+# Start UDP custom service
+/usr/local/bin/udp_service.sh &
+
+# Optional: simple TCP listener on 8080 for banner grabbing
+while true; do echo "Nmap TCP Test Service" | nc -l -p 8080; done &
+
+# Keep container running
+sleep infinity
